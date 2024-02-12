a9696892c9681dd3e34750c256b6bbd01a98840f
Faranheit15 <ffaranm15@gmail.com>
2024-02-12 23:14:00 +0530

    sign in/sign up form

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Netflix-GPT
 
+<img src="https://www.missioncriticalmagazine.com/ext/resources/2023/10/CHatgpt.jpg?1697059739">
+
 A Netflix clone made in React with Tailwind CSS and Google's Firebase Authentication. As an added bonus, OpenAI's GPT-3 has been integrated into the app for super-powers.
 
 ## How to run?
@@ -11,4 +13,7 @@ I have hosted the app. Check the link in the description of the repo. However, i
 <ol>
 <li>Configured a new React app using <code>create-react-app</code>.</li>
 <li>Configured Tailwind CSS into the app.</li>
+<li>Created a Header Component with Netflix logo</li>
+<li>Add the banner image for login page</li>
+<li>Created Sign in/Sign up form</li>
 </ol>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,68 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleisSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
-    <>
+    <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="banner"
         />
       </div>
-    </>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="text-3xl font-bold py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            required
+            className="p-4 my-4 w-full rounded-lg bg-gray-600"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Enter Email Address"
+          required
+          className="p-4 my-4 w-full rounded-lg bg-gray-600"
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          required
+          className="p-4 my-4 w-full rounded-lg bg-gray-600"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4">
+          {isSignInForm ? "New to Netflix?" : "Already have an account?"}
+          <span
+            onClick={toggleisSignInForm}
+            className="cursor-pointer text-gray-600"
+          >
+            {isSignInForm ? " Sign Up Now" : " Sign In"}
+          </span>
+        </p>
+      </form>
+    </div>
   );
 };
 
