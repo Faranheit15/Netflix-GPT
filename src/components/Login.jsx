5d06565a2dc60f84453c4df13b8bd26c5a5a39fb
Faranheit15 <ffaranm15@gmail.com>
2024-02-17 21:33:29 +0530

    deploy

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,13 +25,14 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div className="absolute">
+      <div className="absolute w-full h-full overflow-hidden">
         <img
+          className="w-full h-full object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="banner"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form className="w-3/12 absolute p-12 bg-black my-80 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
         <h1 className="text-3xl font-bold py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
