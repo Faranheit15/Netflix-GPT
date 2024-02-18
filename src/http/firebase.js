c98ae95b95a4e97d2384e4e7067c8c1ee42ce56e
Faranheit15 <ffaranm15@gmail.com>
2024-02-18 17:17:13 +0530

    hiding api keys

--- src/http/firebase.js
+++ src/http/firebase.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional.
 const firebaseConfig = {
-  apiKey: "AIzaSyA0gZRZJ09pfMApVaJ8tZB7ImHITV6UVhQ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "netflix-gpt-70623.firebaseapp.com",
   projectId: "netflix-gpt-70623",
   storageBucket: "netflix-gpt-70623.appspot.com",
