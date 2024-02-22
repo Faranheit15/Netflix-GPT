3214e06adec8c6345ab271f0348e17e128828c02
Faranheit15 <ffaranm15@gmail.com>
2024-02-22 22:40:44 +0530

    adding appid to env

--- src/http/firebase.js
+++ src/http/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   projectId: "netflix-gpt-70623",
   storageBucket: "netflix-gpt-70623.appspot.com",
   messagingSenderId: "591100923561",
-  appId: "1:591100923561:web:ba0e44dea45beb776be3b8",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-WT35CGG8TR",
 };
 
